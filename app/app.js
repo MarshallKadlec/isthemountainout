ee434ce0b958a964a42aae708dd7acdc3bcad35b
Marshall Kadlec <mkadlec13@gmail.com>
2017-06-15 22:18:28 -0700

    clarifai

--- app/app.js
+++ app/app.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const app = express()
 const dateTime = require('node-datetime')
+var clarifai = require('clarifai');
+var sauron = new clarifai.App(
+  'MmmraZ1T8H05NGxbFGRRgZFDbs5StmYgGpuu56QN',
+  'Px7GGwokqNrDKiDSp2iOl7ecWkAcXWTrojvqMha8'
+)
+
+var current_result = false
 
 app.get('/', function (req, res) {
-  var dt = dateTime.create();
-  var formatted = dt.format('Y_m_d/H');
-  var mins = Math.floor((new Date()).getMinutes()/10)*10;
+  res.send(current_result)
+})
+
+var process = function ()
+{
+  var dt = dateTime.create()
+  var formatted = dt.format('Y_m_d/H')
+  var mins = Math.floor((new Date()).getMinutes()/10)*10
   mins = mins < 10 ? '0'+mins : mins
   var url = "https://ismtrainierout.com/timelapse/"+formatted+mins+".jpg"
   
   //$.ajax({type:"GET", url:'/'}).done();
-  res.send(url)
-})
+}
 
 app.listen(3000, function () {
   console.log('The Mountain is listening on port 3000')
