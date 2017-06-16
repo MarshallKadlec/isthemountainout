07cfd266ef9cc7cecde034eaf17af72cbaba6d04
Marshall Kadlec <mkadlec13@gmail.com>
2017-06-16 00:02:06 -0700

    merging

--- app/app.js
+++ app/app.js
@@ -1,43 +1,44 @@
-const express = require('express')
-const app = express()
-const dateTime = require('node-datetime')
-const https = require('https')
+const express = require('express');
+const app = express();
+const dateTime = require('node-datetime');
+const https = require('https');
 var clarifai = require('clarifai');
+const secrets = require('../secrets');
 var sauron = new clarifai.App(
-  'MmmraZ1T8H05NGxbFGRRgZFDbs5StmYgGpuu56QN',
-  'Px7GGwokqNrDKiDSp2iOl7ecWkAcXWTrojvqMha8'
-)
-var current_result = "I dunno!"
+  secrets.clarifai.client_id,
+  secrets.clarifai.client_secret
+);
+var current_result = false;
 
 app.get('/', function (req, res) {
-  res.send(""+(current_result ? "Nope!" : "Yep!"))
+  res.send(""+(current_result ? "Yep!" : "Nope!"));
 })
 
 var process = function ()
 {
-  var dt = dateTime.create()
-  var formatted = dt.format('Y_m_d/H')
-  var mins = Math.floor((new Date()).getMinutes()/10)*10
-  mins = mins < 10 ? '0'+mins : mins
-  var datetime = formatted+mins
-  var url = "https://ismtrainierout.com/timelapse/"+datetime+".jpg"
+  var dt = dateTime.create();
+  var formatted = dt.format('Y_m_d/H');
+  var mins = Math.floor((new Date()).getMinutes()/10)*10;
+  mins = mins < 10 ? '0'+mins : mins;
+  var datetime = formatted+mins;
+  var url = "https://ismtrainierout.com/timelapse/"+datetime+".jpg";
   https.get(url, function(res) {
 	console.log(datetime+": "+res.statusCode);
 	if(res.statusCode == 200)
 	{
 	  sauron.models.predict(Clarifai.GENERAL_MODEL, url).then(
 	    function(res) {
-		  console.log(response.outputs[0].data.concepts)
-		  var found = false
+		  console.log(response.outputs[0].data.concepts);
+		  var found = false;
 		  response.outputs[0].data.concepts.forEach(
 		    function(ele) {
-			  if(ele['name'].toLowerCase() == 'mountain')
+			  if(ele['name'].toLowerCase() == 'mountain');
 			  {
-				found = true
+				found = true;
 			  }
 			}
 		  );
-		  current_result = found
+		  current_result = found;
 	    },
 	    function(err) {
 		  console.error(err);
@@ -49,7 +50,7 @@ var process = function ()
 
 app.listen(3000, function () {
   console.log('The Mountain is listening on port 3000')
-})
+});
 
-setInterval(process, 600000)
-process()
+setInterval(process, 600000);
+process();
